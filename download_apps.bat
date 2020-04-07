97887738c29ea492f2e6af3884f6eae3d3188bcc
Jonathan Lipps <jlipps@gmail.com>
2020-04-07 07:37:37 -0700

    fix windows download apps

--- download_apps.bat
+++ download_apps.bat
@@ -1,5 +1,3 @@
+set curdir=%cd%
 mkdir apps
-bitsadmin /create downloadAppsJob
-bitsadmin /addfile downloadAppsJob https://github.com/cloudgrey-io/the-app/releases/download/v1.10.0/TheApp-v1.10.0.apk apps\TheApp.apk
-bitsadmin /resume downloadAppsJob
-bitsadmin /complete downloadAppsJob
+bitsadmin /transfer downloadAppsJob /dynamic /download /priority FOREGROUND https://github.com/cloudgrey-io/the-app/releases/download/v1.10.0/TheApp-v1.10.0.apk %curdir%\apps\TheApp.apk
